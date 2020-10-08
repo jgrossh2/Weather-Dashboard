eb9305b229ab9ae3db1bc5c0b37200ffde431e96
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-08 12:56:31 -0500

    upi added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var day5 = document.getElementById('date5');
 //dates for weather
 var displayDay= moment().format('L');
 console.log(displayDay);
-currentDayEl.textContent = displayDay;
+// currentDayEl.textContent = displayDay;
 //5 day forecast
 var weekDay1 = moment().add(1, 'days').format('L');
 day1.textContent = weekDay1;
@@ -32,17 +32,19 @@ var cityDisplay = function() {
     // empties html
     $("#cities").empty();
 
-// iterates over the list
-for (var i = 0; i < list.length; i++) {
-    //create new var to hold the p tag
-    var searchCity = $('<p>');
-    searchCity.text(list[i]);
-    console.log("hello");
+    // iterates over the list
+    for (var i = 0; i < list.length; i++) {
+        //create new var to hold the p tag
+        var searchCity = $('<p>');
+        searchCity.text(list[i]);
+        console.log("hello");
 
-    //adds city to the list
-    $("#cities").append(searchCity);
-    console.log("hi");
-}
+        //adds city to the list
+        $("#cities").append(searchCity);
+        console.log("hi");
+    }
+};
+
 $('.btn').on("click", function(event) {
     event.preventDefault();
     console.log("btn");
@@ -54,16 +56,116 @@ $('.btn').on("click", function(event) {
     list.push(city);
     //update list on page
     cityDisplay(list);
-
+    currentDayEl.textContent = city + "(" + displayDay + ")" ;
     //save city in local storage
     localStorage.setItem("cities", JSON.stringify(list));
+    cityWeather(city);
+    cityForecast(city);
 
     //clear search box
-    $("search").val('');
+    $(".form-control").val('');
+    console.log("clear");
 });
-};
 
-cityDisplay()
+var cityWeather = function(city) {
+    // var citySearch  = document.querySelector(".form-control");
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=7d95de6a04331392bb18348d8a3c24c9';
+    fetch (apiUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data, city);
+        var tempEl = document.querySelector('#temp');
+        tempEl.textContent = "Temperature: " + data.main.temp +"*F";
+        var humidEl = document.querySelector('#humid');
+        humidEl.textContent = "Humidity:" + data.main. humidity +"%";
+        var windEl = document.querySelector('#wind');
+        windEl.textContent = "Wind: " + data.wind.speed + "MPH";
+        
+        // var icon = data.weather[0].icon;
+        // var iconDisplay = ;
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        var uvEl = document.querySelector('#uv');
+        var apiUrlUv = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=7d95de6a04331392bb18348d8a3c24c9';
+        fetch (apiUrlUv)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+        var uv = data.value
+        // uv.className("uvColor")
+         uvEl.textContent = "UV Index: " + uv;
+         if (uv > 7 || uv === 7) {
+            $("#uv").addClass("bg-danger")
+        } else if 
+            (uv < 7 && uv > 4) {
+            $("#uv").addClass("bg-warning");
+        } else {
+            $("#uv").addClass("bg-success");
+        }
+        });
+        var upiForecast = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=7d95de6a04331392bb18348d8a3c24c9';
+        fetch (upiForecast) 
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var Temp1El = document.querySelector('.Temp1');
+            Temp1El.textContent = "Temperature: " + data.daily[1].temp.day + "*F";
+            var Temp2El = document.querySelector('.Temp2');
+            Temp2El.textContent = "Temperature: " + data.daily[2].temp.day + "*F";
+            var Temp3El = document.querySelector('.Temp3');
+            Temp3El.textContent = "Temperature: " + data.daily[3].temp.day + "*F";
+            var Temp4El = document.querySelector('.Temp4');
+            Temp4El.textContent = "Temperature: " + data.daily[4].temp.day + "*F";
+            var Temp5El = document.querySelector('.Temp5');
+            Temp5El.textContent = "Temperature: " + data.daily[5].temp.day + "*F";
+            var Humid1El = document.querySelector('.Humid1');
+            Humid1El.textContent = "Humidity: " + data.daily[1].humidity;
+            var Humid2El = document.querySelector('.Humid2');
+            Humid2El.textContent = "Humidity: " + data.daily[2].humidity;
+            var Humid3El = document.querySelector('.Humid3');
+            Humid3El.textContent = "Humidity: " + data.daily[3].humidity;
+            var Humid4El = document.querySelector('.Humid4');
+            Humid4El.textContent = "Humidity: " + data.daily[4].humidity;
+            var Humid5El = document.querySelector('.Humid5');
+            Humid5El.textContent = "Humidity: " + data.daily[5].humidity;
+         });
+        });
+    }
+    // .catch(function(error) {
+    //     //alert if there is a problem
+    //     alert("Unable to find your request.")
+    // })
+
+
+// var cityForecast = function(city) {
+    
+
+//  var cityForecast = function(city) {
+//      var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=7d95de6a04331392bb18348d8a3c24c9';
+//      fetch (apiUrlForecast) 
+//      .then(function(response) {
+//          return response.json();
+//      })
+//      .then(function(data) {
+//         var Temp1El = document.querySelector('.Temp1');
+//         Temp1El.textContent = "Temperature: " + data.list[8].main.temp + "*F";
+//         var Temp2El = document.querySelector('.Temp2');
+//         Temp2El.textContent = "Temperature: " + data.list[16].main.temp + "*F";
+//         var Temp3El = document.querySelector('.Temp3');
+//         Temp3El.textContent = "Temperature: " + data.list[24].main.temp + "*F";
+//         var Temp4El = document.querySelector('.Temp4');
+//         Temp4El.textContent = "Temperature: " + data.list[32].main.temp + "*F";
+//         var Temp5El = document.querySelector('.Temp5');
+//         Temp5El.textContent = "Temperature: " + data.list[40].main.temp + "*F";
+//      })
+//      .catch(function(error) {
+//          alert("Unable to display forecast.")
+//      });
+//  
 
 
 //console log to find display issue
